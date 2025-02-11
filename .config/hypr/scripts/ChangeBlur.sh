500e2d31d90365f174f9ca768d87762cd5346614
Michael Hess <michaelhess17@yahoo.com>
2025-02-11 14:51:34 -0500

    updated weather location and blur settings for hyprland keybinds

--- .config/hypr/scripts/ChangeBlur.sh
+++ .config/hypr/scripts/ChangeBlur.sh
@@ -4,14 +4,18 @@
 
 notif="$HOME/.config/swaync/images/bell.png"
 
-STATE=$(hyprctl -j getoption decoration:blur:passes | jq ".int")
+STATE=$(hyprctl -j getoption decoration:blur:size | jq ".int")
 
-if [ "${STATE}" == "2" ]; then
-	hyprctl keyword decoration:blur:size 2
-	hyprctl keyword decoration:blur:passes 1
- 	notify-send -e -u low -i "$notif" " Less Blur"
-else
+if [ "${STATE}" == "20" ]; then
+	hyprctl keyword decoration:blur:size 0
+	hyprctl keyword decoration:blur:passes 2
+ 	notify-send -e -u low -i "$notif" " Blur Off"
+elif [ "${STATE}" == "0" ]; then
 	hyprctl keyword decoration:blur:size 6
 	hyprctl keyword decoration:blur:passes 2
   	notify-send -e -u low -i "$notif" " Normal Blur"
+else
+    hyprctl keyword decoration:blur:size 20
+    hyprctl keyword decoration:blur:passes 2
+  	notify-send -e -u low -i "$notif" " More Blur"
 fi
